083206c63c28d70625a42e3a93e3b3d1d9697517
First Fractal <firstfractal512@gmail.com>
2023-01-09 15:46:04 -0600

    made the flamethrower to adjust for every boss

--- BossFlamethrower.cs
+++ BossFlamethrower.cs
@@ -22,10 +22,55 @@ namespace BossFlamethrower
 
         public override void AI(NPC npc)
         {
-            if (npc.boss)
+            // get all of the bosses in the list
+            if (npc.boss || npc.type == NPCID.EaterofWorldsHead || npc.type == NPCID.CultistBossClone)
             {
-                Vector2 velocity = new Vector2(8, 0).RotatedBy(npc.rotation+MathHelper.ToRadians(90));
-                Projectile.NewProjectile(npc.GetSource_FromAI(), npc.Center, velocity, ProjectileID.EyeFire, npc.damage/6, 0f, Main.myPlayer);
+                //dont give moon lord a flamethrower, due to the lighting hiding it
+                if (npc.type != NPCID.MoonLordCore && npc.type != NPCID.MoonLordFreeEye && npc.type != NPCID.MoonLordHand && npc.type != NPCID.MoonLordHead && npc.type != NPCID.MoonLordLeechBlob)
+                {
+                    //set the length of the flamethrower
+                    Vector2 velocity = new Vector2(6, 0);
+                    //if the boss is a eye, boss then rotate the flamethrower it like this to make it come out of the eye
+                    if (npc.type == NPCID.EyeofCthulhu || npc.type == NPCID.Retinazer || npc.type == NPCID.Spazmatism)
+                    {
+                        velocity = velocity.RotatedBy(npc.rotation + MathHelper.ToRadians(90));
+                    }
+                    //if the boss is a plantera, then rotate the flamethrower it like this to make it come out of the mouth
+                    else if (npc.type == NPCID.Plantera)
+                    {
+                        velocity = velocity.RotatedBy(npc.rotation + MathHelper.ToRadians(270));
+                    }
+                    //if the boss is a duke fishron, then rotate the flamethrower it like this to make it come out of the mouth
+                    else if (npc.type == NPCID.DukeFishron)
+                    {
+                        velocity = velocity.RotatedBy(npc.rotation * npc.direction);
+                    }
+                    //if the boss is a worm boss, then rotate the flamethrower it like this to make it come out of the mouth
+                    else if (npc.type == NPCID.TheDestroyer || npc.type == NPCID.EaterofWorldsHead)
+                    {
+                        velocity = velocity.RotatedBy(npc.rotation + MathHelper.ToRadians(270));
+                    }
+
+                    //if the boss is a 2D Side boss, then rotate the flamethrower it like this to make it come out of the direction that it is facing at
+                    else if (npc.type == NPCID.Deerclops || npc.type == NPCID.WallofFlesh || npc.type == NPCID.CultistBoss || npc.type == NPCID.CultistBossClone)
+                    {
+                        if (npc.direction == -1)
+                        {
+                            velocity = new Vector2(-velocity.X, velocity.Y);
+                        }
+                    }
+
+                    //for everyone else, make the flamethower point torwards the player
+                    else
+                    {
+                        Player player = Main.player[npc.target];
+                        float deltaX = player.position.X - npc.position.X;
+                        float deltaY = npc.position.Y - player.position.Y;
+                        velocity = velocity.RotatedBy(-Math.Atan2(deltaY, deltaX));
+                    }
+                    //create the flame thrower
+                    Projectile.NewProjectile(npc.GetSource_FromAI(), npc.Center, velocity, ProjectileID.EyeFire, npc.damage / 6, 0f, Main.myPlayer);
+                }
             }
             base.AI(npc);
         }
